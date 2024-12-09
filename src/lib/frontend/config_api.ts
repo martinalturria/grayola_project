57a365d36a1263dfefad3c30810a403d92878bf8
Martin <martinalturria@hotmail.com>
2024-12-09 14:03:09 -0300

    change api router

--- src/lib/frontend/config_api.ts
+++ src/lib/frontend/config_api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+const BASE_URL = "https://grayola-project.vercel.app/api";
 
 export const apiConfig = {
     baseUrl: BASE_URL,
